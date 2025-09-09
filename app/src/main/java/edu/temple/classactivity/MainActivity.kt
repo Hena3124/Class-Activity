8b3a4856beea796f14df4f47fb7b5380c0fdf212
Hena Patel <hena.patel0002@temple.edu>
2025-09-09 16:50:04 -0400

    editing

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,31 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textView = findViewById<TextView>(R.id.TextView)
+        var edittext= findViewById<EditText>(R.id.enterText)
+
+        findViewById<Button>(R.id.clickMe).setOnClickListener {
+
+            if (edittext.text.isEmpty()){
+                textView.text = "Please try again"
+            }
+            else{
+                textView.text = "Hello, ${edittext.text}"
+                edittext.setText("")
+
+            }
+        }
+
     }
 }
